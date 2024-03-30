7a127503d219d337d686377b0c300900591ff14c
Zarina <ibragimovazara13@gmail.com>
2024-03-30 22:10:32 +0500

    timer change

--- functionality.js
+++ functionality.js
@@ -11,6 +11,12 @@ function convertSecondsToDHMS(totalSeconds) {
     return { days, hours, minutes, seconds: remainingSeconds };
 }
 
+const { days, hours, minutes, seconds } = convertSecondsToDHMS(delta);
+let timerElems = document.querySelectorAll('.timerElem .num');
+timerElems[0].innerHTML = twoDigitsNum(days);
+timerElems[1].innerHTML = twoDigitsNum(hours);
+timerElems[2].innerHTML = twoDigitsNum(minutes);
+
 function twoDigitsNum(a) {
     if (a < 10) {
         return `0${a}`;
@@ -18,12 +24,6 @@ function twoDigitsNum(a) {
         return a;
     }
 }
-
-const { days, hours, minutes, seconds } = convertSecondsToDHMS(delta);
-let timerElems = document.querySelectorAll('.timerElem.num');
-timerElems[0].innerHTML = twoDigitsNum(days);
-timerElems[1].innerHTML = twoDigitsNum(hours);
-timerElems[2].innerHTML = twoDigitsNum(minutes);
 
 
 
