9189866a7501fb1965b91e4ba0555c93170ccf58
Zarina <ibragimovazara13@gmail.com>
2024-03-30 22:25:43 +0500

    pop up form html/css

--- functionality.js
+++ functionality.js
@@ -25,26 +25,3 @@ function twoDigitsNum(a) {
     }
 }
 
-
-
-// function convertSecondsToDHMS(seconds) {
-//     const days = Math.floor(seconds / (3600 * 24));
-//     const hours = Math.floor((seconds % (3600 * 24)) / 3600);
-//     const minutes = Math.floor((seconds % 3600) / 60);
-//     const remainingSeconds = Math.floor(seconds % 60);
-//     return { days, hours, minutes, seconds: remainingSeconds };
-// }
-
-// const { days, hours, minutes, seconds } = convertSecondsToDHMS(delta);
-// let timerElems = document.querySelectorAll('.timerElem .num')
-// timerElems[0].innerHTML = twoDigitsNum(days)
-// timerElems[1].innerHTML = twoDigitsNum(hours)
-// timerElems[2].innerHTML = twoDigitsNum(minutes)
-
-// function twoDigitsNum(a) {
-//     if (a < 10) {
-//         return `0${a}`
-//     } else {
-//         return a
-//     }
-// }
